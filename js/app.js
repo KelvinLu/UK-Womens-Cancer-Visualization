bd0b7b4947a0e7d8f79356f1ba8decf61c6ad5b3
Kelvin Lu <klu@ucdavis.edu>
2015-04-19 04:23:51 -0700

    colorbrew steez

--- js/app.js
+++ js/app.js
@@ -25,11 +25,22 @@ main = function() {
     return Math.max.apply(null, d.cancers.map(function(d, i) { return d.percentage; }));
   }));
 
+  var numCancers = Math.max.apply(null, data.map(function(d, i) {
+    return d.cancers.length;
+  }));
+
   var unityScale = d3.scale.linear()
     .domain([0, maxPercent])
     .range([0, barHeight]);
 
-  var colorScale = d3.scale.category10();
+  var opacityScale = d3.scale.pow()
+    .domain([0, maxPercent])
+    .range([0, 1]);
+
+  var colorScale = d3.scale.ordinal()
+    .range(colorbrewer.GnBu[numCancers] || colorbrewer.GnBu[3]);
+
+  var everyOdd = function(d, i){ return i % 2; };
 
   var percentageFormat = d3.format("3.1%");
 
@@ -45,7 +56,15 @@ main = function() {
     .data(data)
     .enter()
     .append("g")
+    .attr("width", width)
+    .attr("height", categoryHeight)
     .attr("transform", function(d, i) { return "translate(0, " + i * categoryHeight + ")"; })
+
+  var categoryBg = category.append("rect")
+    .attr("width", width)
+    .attr("height", categoryHeight)
+    .classed("bg", true)
+    .classed("bg-even", everyOdd);
 
   var charts = category.append("g");
   var figures = category.append("g");
@@ -68,11 +87,26 @@ main = function() {
 
   figures.attr("transform", function(d, i) { return "translate(0, 0)"; });
 
+  var markOrganFigures = function(bodyFigure) {
+    bodyFigure.each(function(d, i) {
+      var cancers = d.cancers, cancer, figure;
+      for (var i = 0; i < cancers.length; ++i) {
+        cancer = cancers[i];
+        if (figure = this.querySelector("#" + cancer.type.toLowerCase() + " path")) {
+          figure.style.fill = colorScale(i);
+          figure.style.fillOpacity = opacityScale(cancer.percentage);
+        }
+      }
+
+    });
+  };
+
   var bodyFigure = figures.append("svg")
     .attr("width", graphWidthOffset)
     .attr("height", categoryHeight)
     .attr("viewBox", "0 0 745 1051")
-    .html(bodyFigureSVG);
+    .html(bodyFigureSVG)
+    .call(markOrganFigures);
 
   // Bar charts
   //
@@ -99,9 +133,10 @@ main = function() {
     .append("text")
     .text(function(d, i) { return d.type; })
     .attr("dx", "5px")
-    .attr("dy", "20px")
+    .attr("dy", "15px")
     .attr("x", function(d, i) { return i * barWidth; })
-    .attr("y", barPadding + barHeight);
+    .attr("y", barPadding + barHeight)
+    .classed("type", true);
 
   cancerPercentageLabel.data(function(d, i) { return d.cancers; })
     .enter()
